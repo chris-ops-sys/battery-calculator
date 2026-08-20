da0dac1636c1e35e1c1dae54d6ee934bd1d3ca79
Chris <sax782366856@gmail.com>
2026-08-20 10:09:59 +0800

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule BatteryCalculator.MixProject do
   def project do
     [
       app: :litrue_battery_calc,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       description: description(),
