3fed1ef3b72a867ccba445a099759dc14db1c55c
Chris <sax782366856@gmail.com>
2026-08-20 09:23:48 +0800

    Create conf.py

---
+++ conf.py
@@ -0,0 +1,14 @@
+project = 'LiTrue Battery Technical Guide'
+author = 'LiTrue Battery Team'
+release = '1.0'
+
+extensions = [
+    'myst_parser',
+]
+
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.md': 'markdown',
+}
+
+html_theme = 'sphinx_rtd_theme'
